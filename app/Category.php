d2e1aeb05c912dac00efb48e798291ac96150197
Simone823 <simone.daglio823@outlook.it>
2022-05-05 09:23:22 +0200

    category model fillable

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    // Fillable
+    protected $fillable = [
+        'name',
+    ];
+
     // Funzione relazione tabella posts
     public function posts() {
         return $this->hasMany('App\Post');
